b092fa57d6c3a42f94dc6f400adc0e2189bfb5ee
Masashi Ozaki <masashi.ozaki.te@renesas.com>
2024-06-16 21:33:58 +0900

    temp

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -4,30 +4,35 @@ import logging
 
 app = Flask(__name__)
 
-@app.route("/status")
-def status():
+@app.route('/status')
+def healthcheck():
     response = app.response_class(
             response=json.dumps({"result":"OK - healthy"}),
             status=200,
             mimetype='application/json'
-            )
+    )
+    app.logger.info('Status request successfull')
+    app.logger.debug('DEBUG message')
     return response
 
-@app.route("/metrics")
+@app.route('/metrics')
 def metrics():
     response = app.response_class(
-            response=json.dumps({"status":"success", "code":0, "data":{"UserCount":140,"UserCountActive":23}}),
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
             status=200,
             mimetype='application/json'
-            )
+    )
+    app.logger.info('Metrics request successfull')
     return response
 
 @app.route("/")
 def hello():
-    app.logger.info("Main request successfull")
-    app.logger.debug("Main request successfull debug")
+    app.logger.info('Main request successfull')
+
     return "Hello World!"
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log', encording='utf-8', level=logging.INFO)
+    ## stream logs to a file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    
     app.run(host='0.0.0.0')
